d556b8cd2ab48eb6dd5153ad7595e4eed9719e8d
Kyle Irwin <kirwi@fedora.attlocal.net>
2025-11-24 18:29:35 -0500

    Initial project structure with FP algorithm modules

---
+++ src/main/scala/sorting/MergeSort.scala
@@ -0,0 +1,11 @@
+package sorting
+
+object MergeSort:
+
+  /** Pure functional merge sort
+   * @param xs List of elements to sort
+   * @return a new sorted List
+   * */
+
+  def mergeSort[A](xs: List[A])(using Ordering[A]): List[A] =
+    ???
